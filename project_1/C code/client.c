b4499687f8e8b6cbdc4754923ef12fdef9a238ce
nschlues <nschlues@uccs.edu>
2025-03-24 19:51:04 -0600

    More work

--- project_1/C code/client.c
+++ project_1/C code/client.c
@@ -12,17 +12,33 @@ after validating certificates with the given server. */
 #define FAIL -1
 
 
-/* Main function to simulate the client sending a request to a server 
+/* Main function to simulate the client sending a request to a server.
 Arguments: Gets the number of arguments as the first argument and the rest of the arguments as an array of strings.
 Returns: idk yet nothing important probably just an exit code.
 */
 int main(int argc, char *argv[]){
+    
+    
+    // Initialize variables
+    char buff[1024];
+
 
     // Check to see if the program was called with the right number of arguments.
     if (argc != 3){
         printf("Improper usage. Expected compiledClient.exe <hostname> <portnum>");
         exit(0);
     }
+
+    // Inside the loop if the SSL_connect is true
+    // Declare the spots for the password and username
+    char username[16] = {0};
+    char password[16] = {0};
+
+    
+    // Print prompts after validation
+    printf("Please enter your username: \n");
+    // scanf and validate for user 
+    printf("Please enter your username: \n");
 
 }
 
