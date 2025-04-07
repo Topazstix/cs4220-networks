9835537119f50d536360c93fd5e0b074391eb08c
nschlues <nschlues@uccs.edu>
2025-04-07 14:28:27 -0600

    Update to server.c
    Got mostly the main function done I think aside from ssl functions

--- project_1/C code/server.c
+++ project_1/C code/server.c
@@ -1,4 +1,5 @@
-/* This program is designed to represent a client server in C. It is setup to communicate securely via open SSL. The client will encrypt and send a message 
+/* This program is designed to represent a server in C. It is setup 
+to communicate securely via open SSL. The client will encrypt and send a message 
 after validating certificates with the given server. */
 
 
@@ -17,24 +18,41 @@ after validating certificates with the given server. */
 
 
 /* Main function to simulate the client sending a request to a server.
-Arguments: idk
+Arguments: Needs the portnumber
 Returns: idk yet nothing important probably just an exit code.
 */
 int main(int argc, char * argv[]){
 
-    /* Definitions */
-
-
     /* Error checking */ 
-    // Check if user is root ( only matters if port is <1000)
-    // Check if called with the right number of arguments
+    // Check if user is root (only matters if port is <1000)
+
+
+    /* Check if right number of arguments */
+    if (argc != 2) {
+
+        printf ("Error: Wrong number of arguments\nUsage: %s <port number>\n", argv[0]);
+        exit(0);
+    }
+
+    /* Definitions */
+    char *portnum = argv[1];
 
 
     /* Start SSL/Socket stuff*/
+    // Handshake phase: client sends request and server sends certificate
+
+    // Client validates certificate 
+
+    // Client creates private session key and sends packet encrypted with server's pubblic key
+
+    // Server now has access to private session key
+
+    // Encrypted communication can start between client and server
 
 
     /* Listening */
     // Infinite listening loop 
+    while (1) {
 
         /* Recieving */
 
@@ -42,20 +60,22 @@ int main(int argc, char * argv[]){
         /* Validating */
 
 
-        /* Transfer Message to User*/
+        /* Transfer Message to User */
+
+
+    }
 
 
     /* End */
+    // Manage memory (probably not needed)
+    free(portnum);
     // Close socket and connections
-
+    // close(server);
 
 }
 
 
 
-
-
 // Other functions as needed
-
 
 
